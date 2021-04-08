3ffcd93c2ee3950fcefcb8d30d98d0de1a895123
Caique <caiqueandrey@hotmail.com>
2021-04-08 19:27:40 -0300

    Corrigindo bug do ssr

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -20,7 +20,7 @@ export function History (){
                             <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z"/>
                             <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
                         </svg>
-                        <strong>Histórico</strong>
+                        <strong>History</strong>
                     </span>
 
                     <span className="spanTwo" onClick={()=> resetHistory()} >
@@ -31,7 +31,7 @@ export function History (){
                 </HeaderHistory>
                 <ListHistory>
                     <ul>
-                    { history.length === 0 && <p>Histórico vazio</p> }
+                    { history.length === 0 && <p>Empty History</p> }
                         {
                             history.map((operation, i)=>{
                                 return (
